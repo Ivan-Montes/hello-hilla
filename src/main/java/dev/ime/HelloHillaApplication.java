0a80151fd200b798c675eb86ee194574d351497b
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-07-19 07:33:44 +0200

    feat: initial commit

---
+++ src/main/java/dev/ime/HelloHillaApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ime/HelloHillaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

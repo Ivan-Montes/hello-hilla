794c2983b1b2fc6787dde40fac761e39d84b623f
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-07-20 08:58:12 +0200

    feat: initial commit hello-hilla-auto

---
+++ hello-hilla-auto/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO suppliers(supplier_name, headquarter_city) VALUES('Agnes Depot', 'Sepulveda');
+INSERT INTO products(name, category, price, date_added, supplier_id) VALUES('Stirrup M ', 'Headphones', 19.9, CURRENT_DATE, 1);
+INSERT INTO products(name, category, price, date_added, supplier_id) VALUES('New Stirrup M', 'Headphones', 25.9, CURRENT_DATE, 1);
+INSERT INTO products(name, category, price, date_added, supplier_id) VALUES('New Action Stirrup M', 'Headphones', 29.9, CURRENT_DATE, 1);

65c821ca59c730e994709c0c3e4748542b4827ba
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-07-30 07:21:08 +0200

    feat: add customers index.tsx

---
+++ hello-hilla-rest/src/main/frontend/views/customers/@index.tsx
@@ -0,0 +1,9 @@
+export default function CustomerListView() {
+    return (
+        <div style={{backgroundColor: 'cyan', height: '500px'}}> 
+            <header>
+                <h3>List of Customers (View)</h3>
+            </header>
+        </div>
+    );
+}
